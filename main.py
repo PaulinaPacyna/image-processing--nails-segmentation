e2d2ef910a37647b382646cdb0fcfa7f7bd829d0
PaulinaPacyna <62997314+PaulinaPacyna@users.noreply.github.com>
2020-12-04 20:43:38 +0100

    hsv tresholding blurring and closing

--- main.py
+++ main.py
@@ -4,44 +4,52 @@ import cv2
 import numpy as np
 
 
-def channel(image, channel=0):
-    if channel == 0:
-        b = image.copy()
-        b[:, :, 1] = 0
-        b[:, :, 2] = 0
-        return b
-    if channel == 0:
-        g = image.copy()
-        g[:, :, 0] = 0
-        g[:, :, 2] = 0
-        return g
-    if channel == 0:
-        r = image.copy()
-        r[:, :, 0] = 0
-        r[:, :, 1] = 0
-        return r
+def side_by_side_4(a, B, C, D):
+    return np.vstack((np.hstack((a, B)), np.hstack((C, D))))
 
 
-def sidebyside4(A, B, C, D):
-    return np.vstack((np.hstack((A, B)), np.hstack((C, D))))
+def side_by_side_6(A, B, C, D, E, F):
+    return np.vstack((np.hstack((A, B)), np.hstack((C, D)), np.hstack((E, F))))
 
 
-os.chdir('./nails_segmentation/images')
-for file in glob.glob('*.jpg'): #np.random.choice(glob.glob("*.jpg"), 3):
-    image = cv2.imread(file)
+def hsv_tresholding(image, lower, upper):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower_hsv1 = np.array([0, 40, 80])
-    upper_hsv1 = np.array([15, 250, 250])
-    lower_hsv2 = np.array([95, 10, 150]) #h,s,v = b,g,r
-    upper_hsv2 = np.array([180, 23, 254])
-    # lower_hsv2 = np.concatenate([np.array([165]),lower_hsv1[1:]])
-    # upper_hsv2 = np.concatenate([np.array([180]),upper_hsv1[1:]])
-    mask_hsv1 = cv2.cvtColor(cv2.inRange(hsv, lower_hsv1, upper_hsv1), cv2.COLOR_GRAY2BGR)
-    mask_hsv2 = cv2.cvtColor(cv2.inRange(hsv, lower_hsv2, upper_hsv2), cv2.COLOR_GRAY2BGR)
-    # lower_image = np.array([10, 10, 10])
-    # upper_image = np.array([255, 40, 40])
-    # mask_image = cv2.cvtColor(cv2.inRange(image, lower_image, upper_image), cv2.COLOR_GRAY2RGB)
-    cv2.namedWindow(file, cv2.WINDOW_NORMAL)
-    cv2.imshow(file, sidebyside4(image, hsv, mask_hsv1, mask_hsv2))
-    cv2.resizeWindow(file,600,600)
-    cv2.waitKey(0)
+    lower_hsv1 = np.array(lower)
+    upper_hsv1 = np.array(upper)
+    return cv2.cvtColor(cv2.inRange(hsv, lower_hsv1, upper_hsv1), cv2.COLOR_GRAY2BGR)
+
+
+def smth():
+    for filename, image in images.items():
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask_hsv1 = hsv_tresholding(image, [0, 25, 80], [20, 220, 250])
+        blurred = cv2.medianBlur(mask_hsv1, 5)
+        opened = cv2.morphologyEx(blurred, cv2.MORPH_OPEN, kernel=np.ones((5, 5)))
+        cv2.namedWindow(filename, cv2.WINDOW_NORMAL)
+        cv2.imshow(
+            filename,
+            side_by_side_6(
+                image,
+                hsv,
+                mask_hsv1,
+                blurred,
+                cv2.morphologyEx(blurred, cv2.MORPH_CLOSE, kernel=np.ones((5, 5))),
+                cv2.morphologyEx(blurred, cv2.MORPH_CLOSE, kernel=np.ones((21, 21))),
+            ),
+        )
+        # cv2.imshow(file, side_by_side_4(image, hsv, mask_hsv1, cv2.bitwise_or(mask_hsv1, mask_hsv2)))
+        cv2.resizeWindow(file, 600, 600)
+        cv2.waitKey(0)
+
+
+images = {}
+test = {}
+
+os.chdir("./nails_segmentation/images")
+for file in glob.glob("*.jpg"):
+    # for file in  np.random.choice(glob.glob("*.jpg"), 3):
+    images[file] = cv2.imread(file)
+os.chdir("../labels")
+for file in glob.glob("*.jpg"):
+    test[file] = cv2.imread(file)
+smth()
---
+++ unused.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+from main import images, test
+
+
+def hvs_tresholding_error(bl, gl, rl, bu, gu, ru):
+    err = 0
+    for filename, image in images.items():
+        thresholded = cv2.inRange(cv2.cvtColor(image, cv2.COLOR_BGR2HSV), np.array([bl, gl, rl]),
+                                  np.array([bu, gu, ru]))
+        ground_truth, b, c = cv2.split(test[filename])
+        err += np.sum((thresholded - ground_truth) ** 2)
+
+
+def channel(image, channel=0):
+    if channel == 0:
+        b = image.copy()
+        b[:, :, 1] = 0
+        b[:, :, 2] = 0
+        return b
+    if channel == 0:
+        g = image.copy()
+        g[:, :, 0] = 0
+        g[:, :, 2] = 0
+        return g
+    if channel == 0:
+        r = image.copy()
+        r[:, :, 0] = 0
+        r[:, :, 1] = 0
+        return r
+
+
+def equalization(image):
+    b, g, r = cv2.split(image)
+    eb = cv2.equalizeHist(b)
+    eg = cv2.equalizeHist(g)
+    er = cv2.equalizeHist(r)
+    return cv2.merge((eb, eg, er))
