cbc433b3dbf15cac4fe43830c3f97643860a11bf
PaulinaPacyna <62997314+PaulinaPacyna@users.noreply.github.com>
2020-12-10 19:07:45 +0100

    hand segmentation DONE

--- main.py
+++ main.py
@@ -4,72 +4,18 @@ import cv2
 import numpy as np
 
 
-def side_by_side_4(a, B, C, D):
-    return np.vstack((np.hstack((a, B)), np.hstack((C, D))))
-
-
-def side_by_side_9(A, B, C, D, E, F, G, H, I):
-    return np.vstack((np.hstack((A, B, C)), np.hstack((D, E, F)), np.hstack((G, H, I))))
-
-
-def side_by_side_6(A, B, C, D, E, F):
-    return np.hstack((np.vstack((A, B)), np.vstack((C, D)), np.vstack((E, F))))
-
-
-def hsv_tresholding(image, lower, upper):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower = np.array(lower)
-    upper = np.array(upper)
-    return cv2.cvtColor(cv2.inRange(hsv, lower, upper), cv2.COLOR_GRAY2BGR)
-
-
-def yuv_tresholding(image, lower, upper):
-    yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-    lower = np.array(lower)
-    upper = np.array(upper)
-    return cv2.cvtColor(cv2.inRange(yuv, lower, upper), cv2.COLOR_GRAY2BGR)
-
-
-def ycbcr_tresholding(image, lower, upper):
-    ycbcr = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
-    lower = np.array(lower)
-    upper = np.array(upper)
-    return cv2.cvtColor(cv2.inRange(ycbcr, lower, upper), cv2.COLOR_GRAY2BGR)
-
-
-def clahe(image):
-    lab = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-    lab_planes = cv2.split(lab)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(3, 3))
-    lab_planes[0] = clahe.apply(lab_planes[0])
-    lab = cv2.merge(lab_planes)
-    return cv2.cvtColor(lab, cv2.COLOR_YUV2BGR)
-
-
-def ycrcb_with_morph(image, lower, upper):
-    size = image.shape[:2]
-    mask = ycbcr_tresholding(image, lower, upper)
-    blurred = cv2.medianBlur(mask, 7)
-    closing_diameter = int(min(image.shape[:2]) / 6)
-    dilation_diameter = int(min(image.shape[:2]) / 15)
-    closed = cv2.morphologyEx(
-        blurred,
-        cv2.MORPH_CLOSE,
-        kernel=cv2.getStructuringElement(
-            cv2.MORPH_ELLIPSE, (closing_diameter, closing_diameter)
-        ),
-    )
-    dilated = cv2.morphologyEx(
-        closed,
-        cv2.MORPH_DILATE,
-        kernel=cv2.getStructuringElement(
-            cv2.MORPH_ELLIPSE, (dilation_diameter, dilation_diameter)
-        ),
-    )
-    return cv2.bitwise_and(image, image, mask=cv2.split(dilated)[0])
-
-
 def side_by_side(*images):
+    def side_by_side_4(a, B, C, D):
+        return np.vstack((np.hstack((a, B)), np.hstack((C, D))))
+
+    def side_by_side_9(A, B, C, D, E, F, G, H, I):
+        return np.vstack(
+            (np.hstack((A, B, C)), np.hstack((D, E, F)), np.hstack((G, H, I)))
+        )
+
+    def side_by_side_6(A, B, C, D, E, F):
+        return np.vstack((np.hstack((A, B, C)), np.hstack((D, E, F))))
+
     for i, img in enumerate(images):
         images = list(images)
         if images[i].ndim == 2:
@@ -77,8 +23,8 @@ def side_by_side(*images):
     if len(images) == 1:
         return images[0]
     if len(images) == 2:
-        return (np.hstack(tuple(images)))
-    zero = np.zeros(images[0].shape,dtype=np.uint8)
+        return np.hstack(tuple(images))
+    zero = np.zeros(images[0].shape, dtype=np.uint8)
     if len(images) <= 4:
         images = images + [zero]
         return side_by_side_4(*images[:4])
@@ -90,82 +36,86 @@ def side_by_side(*images):
         return side_by_side_9(*images[:9])
 
 
-def hsv_carving(image):
-    size = image.shape[:2]
-    mask_hsv1 = hsv_tresholding(image, [0, 30, 80], [20, 220, 255])
-    mask_hsv2 = hsv_tresholding(image, [90, 20, 120], [180, 50, 255])
+def hsv_tresholding(image, lower, upper):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower = np.array(lower)
+    upper = np.array(upper)
+    return cv2.inRange(hsv, lower, upper)
+
+
+def hsv_mask(
+    image,
+    lower1=None,
+    upper1=None,
+    lower2=None,
+    upper2=None,
+    median_blur=5,
+    closing_factor=0.1,
+):
+    if upper2 is None:
+        upper2 = [180, 50, 255]
+    if lower2 is None:
+        lower2 = [115, 14, 120]
+    if upper1 is None:
+        upper1 = [20, 180, 255]
+    if lower1 is None:
+        lower1 = [0, 30, 80]
+    mask_hsv1 = hsv_tresholding(image, lower1, upper1)
+    mask_hsv2 = hsv_tresholding(image, lower2, upper2)
     mask_hsv = cv2.bitwise_or(mask_hsv1, mask_hsv2)
-    blurred = cv2.medianBlur(mask_hsv, 7)
-    closing_diameter = int(min(size) / 6)
-    dilation_diameter = int(min(size) / 15)
-    closed = cv2.morphologyEx(
-        blurred,
-        cv2.MORPH_CLOSE,
-        kernel=cv2.getStructuringElement(
-            cv2.MORPH_ELLIPSE, (closing_diameter, closing_diameter)
-        ),
-    )
-    dilated = cv2.morphologyEx(
-        closed,
-        cv2.MORPH_DILATE,
-        kernel=cv2.getStructuringElement(
-            cv2.MORPH_ELLIPSE, (dilation_diameter, dilation_diameter)
-        ),
-    )
-    carved = cv2.bitwise_and(image, image, mask=cv2.split(dilated)[0])
-    return carved
+    blurred = cv2.medianBlur(mask_hsv, median_blur)
+
+    return mask_hsv
 
 
-def find_hand_old(frame):
-    img = frame.copy()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    YCrCb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
-    # YCrCb_frame = cv2.GaussianBlur(YCrCb_frame, (9, 9), 0)
-    YCrCb_frame = cv2.GaussianBlur(YCrCb_frame, (3, 3), 0)
-    # YCrCb_frame = cv2.GaussianBlur(YCrCb_frame, (1, 1), 0)
-    # cv2.imshow("YCrCb_frame_old", YCrCb_frame)
-    # print(frame.shape[:2])
-    # mask = cv2.inRange(YCrCb_frame, np.array([0, 135, 97]), np.array([255, 177, 127]))#140 170 100 120
-    # mask = cv2.inRange(YCrCb_frame, np.array([0, 133, 77]), np.array([255, 173, 127])) # best enough
-    mask = cv2.inRange(YCrCb_frame, np.array([0, 127, 75]), np.array([255, 177, 130]))
-    bin_mask = mask
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    bin_mask = cv2.dilate(bin_mask, kernel, iterations=5)
-    res = cv2.bitwise_and(frame, frame, mask=bin_mask)
-
-    cv2.imshow("res_old", res)
-
-    return res
+def kmeans(img, K):
+    Z = img.reshape((-1, 3))
+    Z = np.float32(Z)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    return res.reshape(img.shape)
 
 
-def smth():
+def main():
     for filename, image in images.items():
-        image = cv2.GaussianBlur(image, (5, 5), 0)
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        carved = hsv_carving(image)
-        cnn = edges(carved)
         cv2.namedWindow(filename, cv2.WINDOW_NORMAL)
-        h,s,v=cv2.split(hsv)
-        by_side = side_by_side(image, carved, s)
+        final_mask = largest_component_mask(hsv_mask(image))
+        closed = cv2.morphologyEx(
+            final_mask,
+            cv2.MORPH_CLOSE,
+            cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)),
+        )
+        blurred = cv2.medianBlur(closed, 5)
+        component = cv2.bitwise_and(image, image, mask=final_mask)
+
         cv2.imshow(
             filename,
-            by_side
-        )
+            side_by_side(image, component, clahe(component)),
+        ),
+
         cv2.resizeWindow(file, 600, 600)
         cv2.waitKey(0)
 
 
-def edges(carved):
-    hsv = cv2. cvtColor(carved, cv2.COLOR_BGR2HSV)
-    cnn = cv2.Canny(hsv, 0, 255)
-    return cnn
+def largest_component_mask(bin_img):
+    contours = cv2.findContours(bin_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
+    max_area = 0
+    max_contour_index = 0
+    for i, contour in enumerate(contours):
+        contour_area = cv2.moments(contour)["m00"]
+        if contour_area > max_area:
+            max_area = contour_area
+            max_contour_index = i
+    labeled_img = np.zeros(bin_img.shape, dtype=np.uint8)
+    cv2.drawContours(labeled_img, contours, max_contour_index, color=255, thickness=-1)
+    return labeled_img
 
-
-images = {}
-test = {}
 
 os.chdir("./nails_segmentation/images")
+images = {}
+test = {}
 for file in glob.glob("*.jpg"):
     # for file in  np.random.choice(glob.glob("*.jpg"), 3):
     images[file] = cv2.imread(file)
@@ -173,4 +123,4 @@ os.chdir("../labels")
 for file in glob.glob("*.jpg"):
     test[file] = cv2.imread(file)
 
-smth()
+main()
--- unused.py
+++ unused.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-from main import images, test
+from src.main import hsv_mask
 
 
 def hvs_tresholding_error(bl, gl, rl, bu, gu, ru):
@@ -22,12 +22,12 @@ def channel(image, channel=0):
         b[:, :, 1] = 0
         b[:, :, 2] = 0
         return b
-    if channel == 0:
+    if channel == 1:
         g = image.copy()
         g[:, :, 0] = 0
         g[:, :, 2] = 0
         return g
-    if channel == 0:
+    if channel == 2:
         r = image.copy()
         r[:, :, 0] = 0
         r[:, :, 1] = 0
@@ -40,3 +40,110 @@ def equalization(image):
     eg = cv2.equalizeHist(g)
     er = cv2.equalizeHist(r)
     return cv2.merge((eb, eg, er))
+
+
+def find_hand_old(frame):
+
+    YCrCb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
+
+    YCrCb_frame = cv2.GaussianBlur(YCrCb_frame, (3, 3), 0)
+
+    mask = cv2.inRange(YCrCb_frame, np.array([0, 127, 75]), np.array([255, 177, 130]))
+    bin_mask = mask
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    bin_mask = cv2.dilate(bin_mask, kernel, iterations=5)
+    res = cv2.bitwise_and(frame, frame, mask=bin_mask)
+
+    cv2.imshow("res_old", res)
+
+    return res
+
+
+def canny_hsv(carved):
+    hsv = cv2.cvtColor(carved, cv2.COLOR_BGR2HSV)
+    cnn = cv2.Canny(hsv, 0, 255)
+    return cnn
+
+
+def hsv_carving(
+    image,
+    lower1=None,
+    upper1=None,
+    lower2=None,
+    upper2=None,
+    blur=7,
+    cl_factor=6,
+):
+    if upper2 is None:
+        upper2 = [180, 50, 255]
+    if lower2 is None:
+        lower2 = [90, 20, 120]
+    if upper1 is None:
+        upper1 = [20, 220, 255]
+    if lower1 is None:
+        lower1 = [0, 30, 80]
+    size = image.shape[2:]
+    mask_hsv = hsv_mask(image, lower1, lower2, upper1, upper2)
+    blurred = cv2.medianBlur(mask_hsv, blur)
+    closing_diameter = int(min(size) / cl_factor)
+    if closing_diameter > 0:
+        closed = cv2.morphologyEx(
+            blurred,
+            cv2.MORPH_CLOSE,
+            kernel=cv2.getStructuringElement(
+                cv2.MORPH_ELLIPSE, (closing_diameter, closing_diameter)
+            ),
+        )
+        carved = cv2.bitwise_and(image, image, mask=cv2.split(closed)[0])
+        return carved
+    else:
+        m = cv2.split(blurred)[0]
+        return cv2.bitwise_and(image, image, mask=m)
+
+
+def yuv_tresholding(image, lower, upper):
+    yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+    lower = np.array(lower)
+    upper = np.array(upper)
+    return cv2.cvtColor(cv2.inRange(yuv, lower, upper), cv2.COLOR_GRAY2BGR)
+
+
+def ycbcr_tresholding(image, lower, upper):
+    ycbcr = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    lower = np.array(lower)
+    upper = np.array(upper)
+    return cv2.cvtColor(cv2.inRange(ycbcr, lower, upper), cv2.COLOR_GRAY2BGR)
+
+
+def ycrcb_with_morph(image, lower, upper):
+    mask = ycbcr_tresholding(image, lower, upper)
+    blurred = cv2.medianBlur(mask, 7)
+    closing_diameter = int(min(image.shape[:2]) / 6)
+    dilation_diameter = int(min(image.shape[:2]) / 15)
+    closed = cv2.morphologyEx(
+        blurred,
+        cv2.MORPH_CLOSE,
+        kernel=cv2.getStructuringElement(
+            cv2.MORPH_ELLIPSE, (closing_diameter, closing_diameter)
+        ),
+    )
+    dilated = cv2.morphologyEx(
+        closed,
+        cv2.MORPH_DILATE,
+        kernel=cv2.getStructuringElement(
+            cv2.MORPH_ELLIPSE, (dilation_diameter, dilation_diameter)
+        ),
+    )
+    return cv2.bitwise_and(image, image, mask=cv2.split(dilated)[0])
+
+
+def select_biggest_component(mask):
+    mask_ = cv2.split(mask)[0] if mask.ndim == 3 else mask
+    cnts = cv2.findContours(
+        mask_,
+        cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_NONE,
+    )
+    cnts = cnts[0]
+    return sorted(cnts, key=cv2.contourArea, reverse=True)[0]
